0a715134df2ff1749e33d22ba53a16d68dc61231
thangld2407 <thangld2407@gmail.com>
2021-06-11 01:23:20 +0700

    CHECK DBCONNECT

--- dbconnect.php
+++ dbconnect.php
@@ -3,7 +3,7 @@ $host_name = "localhost";
 $db_port = 3306;
 $username = "root";
 $password = "";
-$db_name = "ecomercer";
+$db_name = "clothes";
 $connection = new mysqli($host_name, $username, $password, $db_name,$db_port);
 
 
